843b54dde1bcf1322bc376b49288b6de3305f85a
AugustCarv <gurigonicarv@gmail.com>
2021-09-17 22:48:07 -0300

    meu primeiro commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import { Urna } from './screens/Urna'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Urna />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
